30e26462233a3bffb979f7c92279b05e5c0c679d
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-10-18 01:03:46 +0100

    comlited the exercise

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
     name varchar(100),
     species varchar(100),
     date_of_birth date,
